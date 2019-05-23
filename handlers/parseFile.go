c984321b82363ab8d5b14538bf8880ee45f868a7
gggggggunit <48327163+gggggggunit@users.noreply.github.com>
2019-05-23 23:11:23 +0300

    Add files via upload

---
+++ handlers/parseFile.go
@@ -0,0 +1,21 @@
+package handlers
+
+import "html/template"
+
+var tpl *template.Template
+
+func init(){
+
+	var err error
+
+	tpl, err=template.ParseFiles(
+		"templates/index.html",
+		"templates/header.html",
+		"templates/footer.html",
+		"templates/write.html",
+		)
+
+	if err != nil{
+		panic(err)
+	}
+}
